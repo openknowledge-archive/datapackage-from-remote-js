968798a08cc411444ac655fb7263404ab12da65b
nide <nenegoro@gmail.com>
2015-06-10 14:14:18 +0300

    First test passed

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
+var _ = require('underscore');
 var request = require('superagent');
 
 
 // Query remote endpoint url and map response according passed options
-module.exports = function(url, options) { return new Promise(function(RS, RJ) {
-  request.get(url)
-    .end((function(E, R) {
-      if(E)
-        RJ('End point request failed: ' + E);
+module.exports = function(url, options) {
+  if(_.isUndefined(url) || _.isEmpty(url))
+    throw new Error('URL is required');
 
-      // Mapping routines
-      RS({
-        ckan: {
-          '3.0': {
-            base: function(input) { return {}; },
-            tabular: function(input) { return {}; }
+  return new Promise(function(RS, RJ) {
+    request.get(url)
+      .end((function(E, R) {
+        if(E)
+          RJ('End point request failed: ' + E);
+
+        // Mapping routines
+        RS({
+          ckan: {
+            '3.0': {
+              base: function(input) { return {}; },
+              tabular: function(input) { return {}; }
+            }
           }
-        }
-      });
-    }).bind(this));
-}); }
+        });
+      }).bind(this));
+  });
+}
--- test.js
+++ test.js
@@ -11,8 +11,23 @@ var ENDPOINT_RESPONSE = {};
 describe('Datapackage from remote', function() {
   it('throw exception if no url or empty url passed in params', function(done, err) {
     if(err) done(err);
-    false.should.be.true;
-    done();
+
+    try {
+      fromRemote();
+    } catch(exception) {
+      exception.message.should.be.not.empty;
+      exception.message.should.be.a('string');
+
+      try {
+        fromRemote('');
+      } catch(exception) {
+        exception.message.should.be.not.empty;
+        exception.message.should.be.a('string');
+        done();
+      }
+    }
+
+    return;
   });
 
   it('throw exception if url is invalid', function(done, err) {
