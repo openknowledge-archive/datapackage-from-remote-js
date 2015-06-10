1e6fd23c9073f6cc1e2190f6c179c3c076823a62
nide <nenegoro@gmail.com>
2015-06-10 14:08:50 +0300

    Empty test cases

---
+++ test.js
@@ -0,0 +1,42 @@
+var assert = require('chai').assert;
+var chai = require('chai');
+var fromRemote = require('./index');
+var request = require('superagent');
+var requestMock = require('superagent-mock');
+var should = require('chai').should();
+
+var ENDPOINT_RESPONSE = {};
+
+
+describe('Datapackage from remote', function() {
+  it('throw exception if no url or empty url passed in params', function(done, err) {
+    if(err) done(err);
+    false.should.be.true;
+    done();
+  });
+
+  it('throw exception if url is invalid', function(done, err) {
+    if(err) done(err);
+    false.should.be.true;
+    done();
+  });
+
+
+  it('return Promise object', function(done, err) {
+    if(err) done(err);
+    false.should.be.true;
+    done();
+  });
+
+  it('map CKAN version 3 into base datapackage', function(done, err) {
+    if(err) done(err);
+    false.should.be.true;
+    done();
+  });
+
+  it('map CKAN version 3 into tabular datapackage', function(done, err) {
+    if(err) done(err);
+    false.should.be.true;
+    done();
+  });
+});
