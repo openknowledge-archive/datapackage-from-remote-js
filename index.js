3c308a25647e997cb9283daef94a35ea3b5e0984
richhall <rhllrichard@gmail.com>
2015-10-05 22:52:18 +0100

    potentially resolves okfn/datapackagist/#165
    illustrates potential workaround for DKAN integration

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var _ = require('underscore');
 var async = require('async');
 var csv = require('csv');
 var infer = require('json-table-schema').infer;
-var Promise = require('bluebird');
+var Promise = require('promise-polyfill');
 var request = require('superagent');
 var validator = require('validator');
 
@@ -83,8 +83,62 @@ module.exports = function(url, options) {
                 }, function(E, R) { RS(_.extend(datapackage, {resources: R})); });
               }
             }
+          },
+		dkan: {
+            '3.0': {
+              base: function(input) {
+				
+				//work around DKAN's results array implementation of package_show api
+                var result = input.result[0];
+
+                var datapackage = {
+                  name            : result.name,
+                  title           : result.title,
+                  description     : result.notes,
+                  homepage        : '',
+                  version         : result.version,
+                  licences        : [{id: result.license_id, url: result.license_url}],
+                  author          : _.compact([result.author, result.author_email]).join(' '),
+                  contributors    : [],
+                  sources         : [],
+                  image           : '',
+                  base            : '',
+                  dataDependencies: {},
+                  keywords        : _.pluck(result.tags, 'name')
+                };
+
+
+                // Get each resource in async and infer it's schema
+                async.map(result.resources, function(R, CB) {
+                  var resource = {
+                    hash     : R.hash,
+                    mediatype: R.format,
+                    name     : R.name,
+                    url      : R.url
+                  };
+
+                  var schema = _.isObject(R.schema) && !_.isArray(R.schema) && R.schema;
+
+
+                  // Not sure which exactly .resources[] property specifies mime type
+                  if(!schema && _.contains([R.format, R.mimetype], 'text/csv'))
+                    request.get(R.url).end(function(E, RS) {
+                      csv.parse(RS.text, function(EJ, D) {
+                        if(EJ)
+                          CB(null, resource);
+
+                        CB(null, _.extend(resource, {schema: infer(D[0], _.rest(D))}));
+                      });
+                    });
+
+                  else
+                    CB(null, _.extend(resource, schema && {schema: schema}));
+                }, function(E, R) { RS(_.extend(datapackage, {resources: R})); });
+              }
+            }
           }
-        })[that.options.source][that.options.version][
+        
+		})[that.options.source][that.options.version][
           that.options.datapackage.replace('tabular', 'base')
         ](R.body);
       });
